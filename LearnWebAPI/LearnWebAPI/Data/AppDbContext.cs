98b5e4ea32ed2b6ca42ef26b446306cabd5299ef
Tyon1406 <nguyenquangtruong14062003@gmail.com>
2026-03-19 13:36:59 +0700

    SQL finished

---
+++ LearnWebAPI/LearnWebAPI/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using LearnWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearnWebAPI.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Character> Characters => Set<Character>();
+    }
+}
